d63aa9cefde5cf0587eb5cb7071c9fd4422ea9c0
tefistorari <estefaniastorari14@gmail.com>
2025-10-18 19:02:10 -0300

    Creacion de primeros componentes y paginas
    creacion de componentes nav y paginas home, profile y courses y primeros estilos

--- OpenLearn/src/app/app.routes.ts
+++ OpenLearn/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegisterFormPage } from './pages/register-form-page/register-form-page';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {path: 'registration', component:RegisterFormPage},
-    {path: 'login', component:RegisterFormPage}
+    {path: 'login', component:RegisterFormPage},
+    {path: '', component: Home}
 ];
