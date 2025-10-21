2fe0e69c122cffc15eda7a33f7487f8af588cf91
RamosLaut <lautiram0s@hotmail.com>
2025-10-21 18:01:46 -0300

    myCoursePage: html y estilos

--- OpenLearn/src/app/app.routes.ts
+++ OpenLearn/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RegisterFormPage } from './pages/register-form-page/register-form-page'
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { MyCourses } from './pages/my-courses/my-courses';
+import { CoursesList } from './pages/courses-list/courses-list';
 
 export const routes: Routes = [
     {path: 'registration', component:RegisterFormPage},
     {path: 'login', component: Login},
     {path: 'mycourses', component: MyCourses},
+    {path:'courses', component: CoursesList},
     {path: '', component: Home}
 ];
