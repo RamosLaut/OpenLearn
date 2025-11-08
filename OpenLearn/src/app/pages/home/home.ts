ef0355e29920806044eb8dde21968cd89e928554
Maica Odera <maicaodera97@gmail.com>
2025-11-08 19:20:27 -0300

    Se añadió lógica al botón createCourse en home, de manera que redirige a la página create-course o login según corresponda.

--- OpenLearn/src/app/pages/home/home.ts
+++ OpenLearn/src/app/pages/home/home.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Route, Router, RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
 
+  constructor(private router: Router, private auth: Auth){
+
+  }
+
+  goToCreateCourse(): void {
+
+    if (this.auth.CurrentUserValue) {
+      this.router.navigate(['/courses/new']);
+
+    } else {
+      this.router.navigate(['/login']); 
+    }
+  }
 }
+
